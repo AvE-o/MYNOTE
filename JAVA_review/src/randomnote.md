fd747bc4ae30e84849d24160adb0fd9d0c892c06
AvE-o <tt623457092@gmail.com>
2022-11-04 23:09:43 -0400

    HashMap, Key, Value

--- JAVA_review/src/randomnote.md
+++ JAVA_review/src/randomnote.md
@@ -6,3 +6,7 @@
 int A = 111;
 A % 10 = 1; using the % symbol with 10 to get the last single digit
 A / 10 = 11; using the / symbol with 10 to get rid of last digit
+
+
+Nov 4 ->
+记得 补一下 Map Set
---
+++ Leetcode/src/Hashtable/TwoSum1.java
@@ -0,0 +1,20 @@
+package Hashtable;
+
+public class TwoSum1 {
+    // using Hashtalbe instead of double for loop to improve efficiency
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        // Key,     Value
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                res[0] = i;
+                res[1] = map.get(temp); // get the Value[postion] for nums
+            }
+
+            map.put(nums[i], i); // put value and postion inside the map
+        }
+        return res;
+    }
+}
