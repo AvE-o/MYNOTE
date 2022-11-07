954429326f6729bafb8097de0f99903119d7dd59
AvE-o <tt623457092@gmail.com>
2022-11-07 18:57:51 -0500

    ThreeSum use double pointer

---
+++ Leetcode/src/ThreeSum15.java
@@ -0,0 +1,40 @@
+public class ThreeSum15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // a + b + c = 0
+        List<List<Integer>> result = new ArrayList<>();
+        // 用双指针法
+        // 首先对数组进行排序
+        Arrays.sort(nums);
+
+        // i -> a
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+
+            // 去重a
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if ((nums[i] + nums[left] + nums[right]) > 0) {
+                    right--;
+                } else if ((nums[i] + nums[left] + nums[right]) < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
