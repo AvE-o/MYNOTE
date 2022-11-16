ba28b395798001b44ac6150dafb896cc699703c9
AvE-o <tt623457092@gmail.com>
2022-11-16 12:47:28 -0500

    docs: SubstituteSpace

---
+++ Leetcodemaster/src/reversestring/SubstituteSpace.java
@@ -0,0 +1,53 @@
+package reversestring;
+
+public class SubstituteSpace {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
+
+// 双指针
+class SubstituteSpace1 {
+    public String replaceSpace(String s) {
+        if(s == null || s.length() == 0){
+            return s;
+        }
+        //扩充空间，空格数量2倍
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' '){
+                str.append("  ");
+            }
+        }
+        //若是没有空格直接返回
+        if(str.length() == 0){
+            return s;
+        }
+        //有空格情况 定义两个指针
+        int left = s.length() - 1;//左指针：指向原始字符串最后一个位置
+        s += str.toString();
+        int right = s.length()-1;//右指针：指向扩展字符串的最后一个位置
+        char[] chars = s.toCharArray();
+        while(left>=0){
+            if(chars[left] == ' '){
+                chars[right--] = '0';
+                chars[right--] = '2';
+                chars[right] = '%';
+            }else{
+                chars[right] = chars[left];
+            }
+            left--;
+            right--;
+        }
+        return new String(chars);
+    }
+
+}
