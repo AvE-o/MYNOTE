cf0c04c5e4fc3d9e59d95417001ebe18d36bc873
AvE-o <tt623457092@gmail.com>
2022-11-23 22:59:04 -0500

    docs: day3 review

---
+++ LeetcodemasterReview/src/array/day3/Squareofsortedarray.java
@@ -0,0 +1,26 @@
+package array.day3;
+
+public class Squareofsortedarray {
+}
+
+class SquaresofaSortedArray977 {
+    // double pointer
+    public int[] sortedSquares(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        int index = nums.length - 1;
+
+        // creat a new array
+        int[] result = new int[nums.length];
+        while (i <= j) {
+            if (nums[i] * nums[i] <= nums[j] * nums[j]) {
+                result[index--] = nums[j] * nums[j];
+                j--;
+            } else {
+                result[index--] = nums[i] * nums[i];
+                i++;
+            }
+        }
+        return result;
+    }
+}
