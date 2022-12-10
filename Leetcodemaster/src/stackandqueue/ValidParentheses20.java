c53a527a6b3e244c2379b497775af3a244c215cf
AvE-o <tt623457092@gmail.com>
2022-12-09 23:07:08 -0500

    stack&queue

---
+++ Leetcodemaster/src/stackandqueue/ValidParentheses20.java
@@ -0,0 +1,26 @@
+package stackandqueue;
+
+public class ValidParentheses20 {
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+
+            if (ch == '(') {
+                deque.push(')');
+            } else if (ch == '{') {
+                deque.push('}');
+            } else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            } else {
+                deque.pop();
+            }
+        }
+
+        return true;
+    }
+}
